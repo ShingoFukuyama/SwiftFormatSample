1208875ffcb2d8c1297106fcf61b0541aad7e634
ShingoFukuyama <houseshou@gmail.com>
2022-06-21 11:16:18 +0900

    Initial Commit

---
+++ SwiftFormatSample/SwiftFormatSampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftFormatSampleApp.swift
+//  SwiftFormatSample
+//
+//  Created by Shingo Fukuyama on 2022/06/21.
+//
+
+import SwiftUI
+
+@main
+struct SwiftFormatSampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
